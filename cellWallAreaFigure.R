d9e457c3da19ffb0434b9f2d41946ad60d6a5f27
TTRademacher <rademacher.tim@gmail.com>
2021-03-24 17:26:41 -0400

    Started improving cell-wall area plot to include fraction of ring grown at several points in time, etc.

--- cellWallAreaFigure.R
+++ cellWallAreaFigure.R
@@ -11,18 +11,65 @@ source ('plotingFunctions.R')
 #----------------------------------------------------------------------------------------
 layout (matrix (1:4), height = c (1,1,1,1.3))
 par (mar = c (1, 5, 1, 1))
-tempData <- anatomicalData %>% filter (YEAR == 2018, PLOT  %in% c (5, 1))
-plot (x = tempData [['RRADDISTR']] [tempData [['sampleHeight']] == 4.0 & tempData [['PLOT']]== 5],
-      y = tempData [['CWA']] [tempData [['sampleHeight']] == 4.0 & tempData [['PLOT']]== 5],
-      col = addOpacity (tColours [['colour']] [5], 0.5), pch = 5, xlab = '', 
+tempData <- anatomicalData %>% filter (YEAR == 2018, PLOT  %in% c (5, 1), sampleHeight == 4.0)
+plot (x = tempData [['RRADDISTR']] [tempData [['PLOT']] == 5],
+      y = tempData [['CWA']] [tempData [['PLOT']] == 5],
+      col = 'white', xlab = '', 
       ylab = expression (paste ('cell-wall area (',mu, m^2,')', sep = '')),
       xlim = c (0, 100), ylim = c (0, 1400), axes = FALSE)
-points (x = tempData [['RRADDISTR']] [tempData [['sampleHeight']] == 4.0 & tempData [['PLOT']] == 1],
-        y = tempData [['CWA']] [tempData [['sampleHeight']] == 4.0 & tempData [['PLOT']] == 1],
-        col = addOpacity (tColours [['colour']] [1], 0.3), pch = 19)  
+
+# Add the average portion formed before, during and after the experiment
+FF <- tempData %>% filter (PLOT == 5, period <= as_date ('2018-06-25')) %>% group_by (TREE) %>% 
+  summarise (maxFF = max (RRADDISTR, na.rm = TRUE)) 
+rect (ytop = 1500, ybottom = -100, 
+      xleft  = mean (FF [['maxFF']]) - se (FF [['maxFF']]),
+      xright = mean (FF [['maxFF']]) + se (FF [['maxFF']]),
+      col = addOpacity (tColours [['colour']] [5], 0.3), lty = 0)
+abline (v = mean (FF [['maxFF']]), col = tColours [['colour']] [5], lty = 3, lwd = 2)
+FF <- tempData %>% filter (PLOT == 1, period <= as_date ('2018-06-25')) %>% group_by (TREE) %>% 
+  summarise (maxFF = max (RRADDISTR, na.rm = TRUE)) 
+rect (ytop = 1500, ybottom = -100, 
+      xleft  = mean (FF [['maxFF']]) - se (FF [['maxFF']]),
+      xright = mean (FF [['maxFF']]) + se (FF [['maxFF']]),
+      col = addOpacity (tColours [['colour']] [1], 0.3), lty = 0)
+abline (v = mean (FF [['maxFF']]), col = tColours [['colour']] [1], lty = 3, lwd = 2)
+FF <- tempData %>% filter (PLOT == 5, period <= as_date ('2018-09-03')) %>% group_by (TREE) %>% 
+  summarise (maxFF = max (RRADDISTR, na.rm = TRUE)) 
+rect (ytop = 1500, ybottom = -100, 
+      xleft  = mean (FF [['maxFF']]) - se (FF [['maxFF']]),
+      xright = mean (FF [['maxFF']]) + se (FF [['maxFF']]),
+      col = addOpacity (tColours [['colour']] [5], 0.3), lty = 0)
+abline (v = mean (FF [['maxFF']]), col = tColours [['colour']] [5], lty = 4, lwd = 2)
+FF <- tempData %>% filter (PLOT == 1, period <= as_date ('2018-09-03')) %>% group_by (TREE) %>% 
+  summarise (maxFF = max (RRADDISTR, na.rm = TRUE)) 
+rect (ytop = 1500, ybottom = -100, 
+      xleft  = mean (FF [['maxFF']]) - se (FF [['maxFF']]),
+      xright = mean (FF [['maxFF']]) + se (FF [['maxFF']]),
+      col = addOpacity (tColours [['colour']] [1], 0.3), lty = 0)
+abline (v = mean (FF [['maxFF']]), col = tColours [['colour']] [1], lty = 4, lwd = 2)
+
+# Add individual points
+points (x = tempData [['RRADDISTR']] [tempData [['PLOT']] == 5],
+        y = tempData [['CWA']] [tempData [['PLOT']] == 5],
+        col = addOpacity (tColours [['colour']] [5], 0.2), pch = 5)
+points (x = tempData [['RRADDISTR']] [tempData [['PLOT']] == 1],
+        y = tempData [['CWA']] [tempData [['PLOT']] == 1],
+        col = addOpacity (tColours [['colour']] [1], 0.2), pch = 19)  
 axis (side = 1, at = seq (0, 100, 10), labels = rep ('', 11))
 axis (side = 2, at = seq (0, 1400, 400), las = 1)
-# Add the average portion formed before, during and after the experiment
+
+# Add smoothed mean signal
+tempData <- tempData %>% mutate (bin = cut (RRADDISTR, seq (0, 100, 1)))
+meanCWA <- tempData %>% filter (PLOT == 1) %>% 
+  group_by (bin) %>% summarise (meanCWA = mean (CWA, na.rm = TRUE))
+lines (x = seq (0.5, 99.5, 1), 
+       y = rollmean (meanCWA [['meanCWA']], k = 10, na.pad = TRUE), 
+       col = tColours [['colour']] [1], lwd  = 2)
+meanCWA <- tempData %>% filter (PLOT == 5) %>% 
+  group_by (bin) %>% summarise (meanCWA = mean (CWA, na.rm = TRUE)) 
+lines (x = seq (0.5, 99.5, 1), 
+       y = rollmean (meanCWA [['meanCWA']], k = 10, na.pad = TRUE), 
+       col = tColours [['colour']] [5], lwd = 2, lty = 2)
 
 # Add panel for 2.5 m
 par (mar = c (1,5,1,1))
