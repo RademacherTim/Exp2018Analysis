f9390483fcbd0ab3d9a423b60df07c10f9938124
TTRademacher <rademacher.tim@gmail.com>
2021-03-31 16:06:56 -0400

    Changed water potential figure to exclude the compression treatment.

--- waterPotentialFigures.R
+++ waterPotentialFigures.R
@@ -25,21 +25,23 @@ phi <- phi %>% mutate (week = cut (date, '1 week')) %>% mutate (week = as_date (
 # make boxplots by date
 #----------------------------------------------------------------------------------------
 par (mar = c (5, 6, 1, 1))
-boxplot (phi.branch / 10.0 ~ treatment, data = phi [phi [['week']] < as_date ('2018-10-01'), ], 
+boxplot (phi.branch ~ treatment, data = phi [phi [['week']] < as_date ('2018-10-01') &
+                                             phi [['treatment']] != 4, ], 
          horizontal = TRUE, 
-         col = addOpacity (tColours [['colour']] [c (1, 4, 5)], 0.5),
-         border = tColours [['colour']] [c (1, 4, 5)],
-         ylim = c (0, -1), yaxt = 'n', xlim = c (0, 8), frame = FALSE,
+         col = addOpacity (tColours [['colour']] [c (1, 5)], 0.5),
+         border = tColours [['colour']] [c (1, 5)],
+         ylim = c (0, -1), yaxt = 'n', xlim = c (0, 6), frame = FALSE,
          xlab = expression (paste (phi,'(MPa)', sep = '')), ylab = '', 
          out.pch = 19, out.col = addOpacity (tColours [['colour']] [c (1, 4, 5)], 0.7))
-boxplot (phi.needles / 10.0 ~ treatment, data = phi [phi [['week']] < as_date ('2018-10-01'), ], 
-         horizontal = TRUE, add = TRUE, at = 5:7,
-         col = addOpacity (tColours [['colour']] [c (1, 4, 5)], 0.5),
-         border = tColours [['colour']] [c (1, 4, 5)],
+boxplot (phi.needles ~ treatment, data = phi [phi [['week']] < as_date ('2018-10-01') &
+                                              phi [['treatment']] != 4, ], 
+         horizontal = TRUE, add = TRUE, at = 4:5,
+         col = addOpacity (tColours [['colour']] [c (1, 5)], 0.5),
+         border = tColours [['colour']] [c (1, 5)],
          ylim = c (0, -1), axes = FALSE, frame = FALSE)
-axis (side = 2, at = c (1:3,5:7), labels = rep (c ('control','compressed','chilled'), 2),
+axis (side = 2, at = c (1:2,4:5), labels = rep (c ('control','chilled'), 2),
       las = 1)
-abline (h = 4, col = '#666666')
+abline (h = 3, col = '#666666')
 text (labels = c ('branches', 'needles'), x = -0.9, y = c (0.5, 4.5), col = '#666666', 
       cex = 1.5)
 #========================================================================================
